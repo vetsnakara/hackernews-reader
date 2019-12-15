0921f164c9589a86972333c2faadae704382aa2d
Konstantin Arakantsev <kostya3312@gmail.com>
2019-12-15 20:19:23 +0300

    Add httpService

---
+++ src/services/httpService.js
@@ -0,0 +1,48 @@
+import axios from "axios";
+
+const API_ROOT = process.env.URL || "http://localhost:3000";
+
+const TIMEOUT = 20000;
+
+const HEADERS = {
+  "Content-Type": "application/json",
+  Accept: "application/json"
+};
+
+class HttpService {
+  constructor({
+    baseURL = API_ROOT,
+    timeout = TIMEOUT,
+    headers = HEADERS,
+    auth
+  }) {
+    this.client = axios.create({ baseURL, timeout, headers, auth });
+    this.client.interceptors.response.use(this.handleSuccess, this.handleError);
+  }
+
+  handleSuccess(response) {
+    return response;
+  }
+
+  handleError(error) {
+    return Promise.reject(error);
+  }
+
+  post(path, payload) {
+    return this.client.post(path, payload).then(response => response.data);
+  }
+
+  put(path, payload) {
+    return this.client.put(path, payload).then(response => response.data);
+  }
+
+  patch(path, payload) {
+    return this.client.patch(path, payload).then(response => response.data);
+  }
+
+  delete(path) {
+    return this.client.delete(path).then(response => response.data);
+  }
+}
+
+export default HttpService;
